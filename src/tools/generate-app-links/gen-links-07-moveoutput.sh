e9a6a86b7f8e36c26d95ffa2519f6be5e2238a8f
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2015-11-01 17:58:01 +0000

    Removed empty folders in applinks.d Added a generated Makefile in applinks.d Modified the final applinks.d script to generate a Makefile

--- src/tools/generate-app-links/gen-links-07-moveoutput.sh
+++ src/tools/generate-app-links/gen-links-07-moveoutput.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
 
-mv out-06 ../../../etc/applinks.d
+echo "Moving files to local etc directory..."
+#FIXME mv out-06 ../../../etc/applinks.d
+echo "Done."
+
+echo "Generating Makefile for 'make install'..."
+echo "install:" > ../../../etc/applinks.d/Makefile
+
+cd ../../../etc/applinks.d
+for dir in *
+do
+  if [ -d "$dir" ]
+  then
+    echo -e "\tmkdir -p \$(DESTDIR)/etc/firejail/applinks.d/${dir}" >> Makefile
+	  echo -e "\tinstall -c -m 0644 ${dir}/main \$(DESTDIR)/etc/firejail/applinks.d/${dir}/." >> Makefile
+	  echo -e "\tinstall -c -m 0644 ${dir}/linked \$(DESTDIR)/etc/firejail/applinks.d/${dir}/." >> Makefile
+  fi
+done
 echo "Done."
